6ed6663ba27f3c35fd1f4da51783003d9155a6db
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2024-01-18 12:45:40 -0500

    Comp Sci Major v2

---
+++ my infor v2.py
@@ -0,0 +1,20 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/Smith/AppData/Local/Programs/Python/Python312/myinfo.py ==
+ 
+my name is jalen Smith
+ 
+my address line is Union 07083 New Jersey
+ 
+my number is 9736092907
+>>> 
+============== RESTART: C:/Users/Smith/OneDrive/Desktop/myinfo.py ==============
+ 
+my name is jalen Smith
+ 
+my address line is Union 07083 New Jersey
+ 
+my number is 9736092907
+ 
+my infor v 2
