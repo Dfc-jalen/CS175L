1ce12b2d004111afc53a1b9e822a1eaabe81755e
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2024-01-18 12:43:28 -0500

    Computer Science major
    I am a comp sci major

---
+++ myinfo.py
@@ -0,0 +1,11 @@
+#CS 175L
+#Jalen Smith
+#Myinfo
+print(' ')
+print('my name is','jalen Smith')
+print(' ')
+print('my address line is','Union','07083', 'New Jersey')
+print(' ')
+print('my number is','9736092907')
+
+
